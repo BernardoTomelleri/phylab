5f858ba22c807ecd9ecca8208834b36ab257100e
BernardoTomelleri <bernardo.tomelleri@gmail.com>
2021-03-01 00:14:44 +0100

    Build and Deploy 1st test

---
+++ phylab/__init__.py
@@ -0,0 +1,9 @@
+
+__pkgname__ = 'phylab'
+__version__ = '0.2.1'
+__author__ = 'Bernardo Tomelleri'
+__license__ = 'GPLv3'
+__summary__ = 'A Python module for basic data analysis and curve fitting for Physics laboratory students.'
+__url__ = 'https://github.com/BernardoTomelleri/phylab'
+
+from . import lab
