1349f2f4e6db7657521da571d7f7938ce4760464
BernardoTomelleri <bernardo.tomelleri@gmail.com>
2021-03-12 19:56:03 +0100

    prepare for metastable release

--- phylab/__init__.py
+++ phylab/__init__.py
@@ -1,6 +1,6 @@
 
 __pkgname__ = 'phylab'
-__version__ = '0.2.3'
+__version__ = '0.2.4a'
 __author__ = 'Bernardo Tomelleri'
 __license__ = 'GPLv3'
 __summary__ = 'A Python module for basic data analysis and curve fitting for Physics laboratory students.'
